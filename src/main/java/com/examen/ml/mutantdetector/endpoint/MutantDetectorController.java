7d22a50e5025d7a2b7d6c716a2cc101d6fb6ef38
Marcelo <m.d.sola@gmail.com>
2019-08-07 21:21:42 -0300

    Refinamiento de documentacion y descipcion. Implementacion de manejo codigos de respuesta

--- src/main/java/com/examen/ml/mutantdetector/endpoint/MutantDetectorController.java
+++ src/main/java/com/examen/ml/mutantdetector/endpoint/MutantDetectorController.java
@@ -2,16 +2,27 @@ package com.examen.ml.mutantdetector.endpoint;
 
 import com.examen.ml.mutantdetector.business.DNAMutantValidator;
 import com.examen.ml.mutantdetector.entity.DNARequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
 
 @RestController
+@Api(value = "Servicio Validador de ADN Mutante", description = "Este servicio permite validar si el ADN dado es mutante", tags = {"Validador de ADN Mutante"})
 public class MutantDetectorController {
 
-    @RequestMapping("/mutant")
-    @PostMapping
-    public boolean mutant(DNARequest dnaRequest){
-        return (new DNAMutantValidator()).isMutant(dnaRequest.getDna());
+    @PostMapping(value = "/mutant", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Punto de entrada del validador", notes="Este servicio permite validar si el ADN dado es mutante")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Mutante"),
+            @ApiResponse(code = 403, message = "No mutante")
+    })
+    public ResponseEntity mutant(@RequestBody @ApiParam(value = "Contenedor de ADN", required = true) DNARequest dnaRequest){
+        if(!(new DNAMutantValidator()).isMutant(dnaRequest.getDna()))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        return ResponseEntity.ok().body(null);
     }
+
 }
--- src/main/java/com/examen/ml/mutantdetector/entity/DNARequest.java
+++ src/main/java/com/examen/ml/mutantdetector/entity/DNARequest.java
@@ -1,8 +1,10 @@
 package com.examen.ml.mutantdetector.entity;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 @Data
+@ApiModel()
 public class DNARequest {
     private String[] dna;
 }
