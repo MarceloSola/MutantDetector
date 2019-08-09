6472793ce7eb5201b2e737ef4ad3cc915e485f8b
Marcelo <m.d.sola@gmail.com>
2019-08-09 00:43:30 -0300

    table entity definicion

--- src/main/java/com/examen/ml/mutantdetector/entity/DNARequest.java
+++ src/main/java/com/examen/ml/mutantdetector/entity/DNARequest.java
@@ -4,9 +4,11 @@ import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @Data
 @ApiModel()
 @ToString
-public class DNARequest {
+public class DNARequest implements Serializable {
     private String[] dna;
 }
--- src/main/java/com/examen/ml/mutantdetector/entity/DNAValidationReg.java
+++ src/main/java/com/examen/ml/mutantdetector/entity/DNAValidationReg.java
@@ -2,15 +2,21 @@ package com.examen.ml.mutantdetector.entity;
 
 import lombok.Data;
 import lombok.ToString;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "DNAValidations")
 @ToString
 public class DNAValidationReg {
 
     @Id
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
     private DNARequest dnaRequest;
     private boolean isMutant;
 
